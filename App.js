3df1771be7632c2cdd92c83187ea578e907fac59
João Victor <reflixplayshd@gmail.com>
2022-07-18 01:09:01 -0300

    CurrentPrice and start Graphic

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+import CurrentPrice from './src/components/CurrentPrice';
+import HistoryGraphic from './src/components/HistoryGraphic';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor="#f50d41" barStyle="dark-content" />
+      <CurrentPrice />
+      <HistoryGraphic />
     </View>
   );
 }
@@ -13,8 +16,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 40 : 0,
+  },
+  text: {
+    color: '#f50d41',
   },
 });
